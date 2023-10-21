38f6f1471b264540d7e7c9fc16e6a7d61fda8ca4
YJ Soon <yjsoon@tinkertanker.com>
2023-10-21 16:40:30 +0800

    Added firebase package

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import React from "react";
+import NotesScreenAdd from "./screens/NotesScreenAdd";
+import NotesScreenHome from "./screens/NotesScreenHome";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={NotesScreenHome}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Add"
+          component={NotesScreenAdd}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
